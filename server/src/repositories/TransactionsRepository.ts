6aa6ea0a72a6a0ed1a5f71e3a2ee7077cb327d36
hiagopsilva <hiagohps360@gmail.com>
2020-09-04 15:44:30 -0300

    Initial commit desafio 06

--- server/src/repositories/TransactionsRepository.ts
+++ server/src/repositories/TransactionsRepository.ts
@@ -1,3 +1,5 @@
+import { EntityRepository, Repository } from 'typeorm';
+
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -6,58 +8,10 @@ interface Balance {
   total: number;
 }
 
-interface CreateTransaction {
-  title: string;
-  value: number;
-  type: 'income' | 'outcome';
-}
-
-class TransactionsRepository {
-  private transactions: Transaction[];
-
-  constructor() {
-    this.transactions = [];
-  }
-
-  public all(): Transaction[] {
-    return this.transactions;
-  }
-
-  public getBalance(): Balance {
-    const { income, outcome } = this.transactions.reduce(
-      (accumulator, transaction) => {
-        switch (transaction.type) {
-          case 'income':
-            accumulator.income += transaction.value;
-            break;
-
-          case 'outcome':
-            accumulator.outcome += transaction.value;
-            break;
-
-          default:
-            break;
-        }
-
-        return accumulator;
-      },
-      {
-        income: 0,
-        outcome: 0,
-      },
-    );
-
-    const total = income - outcome;
-
-    return { income, outcome, total };
-  }
-
-  public create({ title, value, type }: CreateTransaction): Transaction {
-    const transaction = new Transaction({ title, value, type });
-
-    this.transactions.push(transaction);
-
-    return transaction;
+@EntityRepository(Transaction)
+class TransactionsRepository extends Repository<Transaction> {
+  public async getBalance(): Promise<Balance> {
+    // TODO
   }
 }
 
